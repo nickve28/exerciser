429f07678b4e2ade4c15342ddb6cdce83c8e803d
nickve28 <nickvaneyk@gmail.com>
2016-12-30 14:18:55 +0100

    added release upgrade step

--- simple_deploy.py
+++ simple_deploy.py
@@ -3,6 +3,9 @@ import os
 import fileinput
 import sys
 
+RELEASE = 1
+UPGRADE = 2
+
 def run_migrations():
     call(["MIX_ENV=prod mix ecto.create && mix ecto.migrate"], shell=True)
 
@@ -10,6 +13,7 @@ def build_fe():
     call(["npm", "run", "--if-present", "deploy"], cwd="./apps/api")
 
 def increase_number(file_name):
+    new_version = None
     new_version_line = None
     with open(file_name, 'r') as file:
         version_line = None
@@ -23,8 +27,24 @@ def increase_number(file_name):
         if 'version' in line:
             line = new_version_line + "\n"
         sys.stdout.write(line)
+    return str(new_version)
+
+def build_release(type):
+    if type == UPGRADE:
+      call(["MIX_ENV=prod mix release --upgrade"], shell=True)
+    else:
+      call(["MIX_ENV=prod mix release"], shell=True)
+
+def deploy_version(new_version, type):
+    cmd = "MIX_ENV=prod PORT=4000 _build/prod/rel/exerciser/bin/exerciser console"
+    if type == UPGRADE:
+      cmd = "MIX_ENV=prod _build/prod/rel/exerciser/bin/exerciser upgrade %s" % str(new_version)
+    call([cmd], shell=True, cwd="./")
 
 if __name__ == '__main__':
+    type = UPGRADE if ('upgrade' in  sys.argv) else RELEASE
     run_migrations()
     build_fe()
-    increase_number('rel/config.exs')
+    new_version = increase_number('rel/config.exs')
+    build_release(type)
+    deploy_version(new_version, type)
