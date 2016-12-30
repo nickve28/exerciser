0d53ccc9bf50fe89c2f6f83b49b3e96e3c5914b0
nickve28 <nickvaneyk@gmail.com>
2016-12-30 13:52:42 +0100

    Added python deploy helper script

---
+++ simple_deploy.py
@@ -0,0 +1,30 @@
+from subprocess import call
+import os
+import fileinput
+import sys
+
+def run_migrations():
+    call(["mix", "ecto.create", "&&", "mix", "ecto.migrate"])
+
+def build_fe():
+    call(["./node_modules/brunch/bin/brunch", "build", "-p"], cwd="./apps/api")
+
+def increase_number(file_name):
+    new_version_line = None
+    with open(file_name, 'r') as file:
+        version_line = None
+        for line in file.readlines():
+            if 'version' in line:
+                version_line = line
+        cur_version = version_line[-3]
+        new_version = int(cur_version) + 1
+        new_version_line = version_line[:-3] + str(new_version) + "\""
+    for line in fileinput.input(file_name, inplace=True):
+        if 'version' in line:
+            line = new_version_line + "\n"
+        sys.stdout.write(line)
+
+if __name__ == '__main__':
+    run_migrations()
+    build_fe()
+    increase_number('rel/config.exs')
