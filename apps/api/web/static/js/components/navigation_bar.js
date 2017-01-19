dde80093ed3904a0b8817a176f23ba0baa42cb4f
nickve28 <nickvaneyk@gmail.com>
2017-01-19 20:37:46 +0100

    Moved content slightly more down

--- apps/api/web/static/js/components/navigation_bar.js
+++ apps/api/web/static/js/components/navigation_bar.js
@@ -3,15 +3,13 @@ import { Link } from 'react-router'
 
 import _ from 'lodash'
 
-import {Paper} from 'material-ui'
-
 export default ({user}) => {
   return (
     <div>
       <div className="color-bar" />
       <nav className="navbar-app" id="navigation">
         <div>
-          <ul className="nav navbar-nav ">
+          <ul className="nav navbar-nav">
             <li><Link to={'/'}>Exercises</Link></li>
             <li><Link to={'/workouts'}>Workouts</Link></li>
             <li className="float-right">
