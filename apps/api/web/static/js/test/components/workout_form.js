364c1851b7ce7b0e05e8a680d09a14b3db688d4d
nickve28 <nickvaneyk@gmail.com>
2017-01-22 15:49:10 +0100

    Added a few workout form tests

--- apps/api/web/static/js/test/components/workout_form.js
+++ apps/api/web/static/js/test/components/workout_form.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import chai from 'chai'
 import chaiEnzyme from 'chai-enzyme'
+import sinon from 'sinon'
+
+import _ from 'lodash'
 
 import LoginForm from '../../components/workout_form';
 import {shallowRender} from '../helpers/theme_helper'
@@ -9,31 +12,65 @@ chai.use(chaiEnzyme())
 const expect = chai.expect
 
 describe('<WorkoutForm />', () => {
-  describe('when action=create', function () {
-    it('should set the text to create', function () {
-      const wrapper = shallowRender(<LoginForm action="create" />)
+  describe('when action=Create', () => {
+    it('should set the text to Create', () => {
+      const wrapper = shallowRender(<LoginForm action="Create" />)
       const field = wrapper.find('h3')
-      expect(field.text()).to.contain('create')
+      expect(field.text()).to.contain('Create')
     })
 
-    it('should set the button text to create', function () {
-      const wrapper = shallowRender(<LoginForm action="create" />)
+    it('should set the button text to Create', () => {
+      const wrapper = shallowRender(<LoginForm action="Create" />)
       const field = wrapper.find('[type="submit"]')
-      expect(field.prop('label')).to.eql('create')
+      expect(field.prop('label')).to.eql('Create')
+    })
+
+    it('should show the load template link', () => {
+      const wrapper = shallowRender(<LoginForm action="Create" />)
+      const field = wrapper.find('small')
+      expect(
+        field.find('a').exists()
+      ).to.eql(true)
+    })
+
+    it('should load the template when clicked', () => {
+      const wrapper = shallowRender(<LoginForm action="Create" />)
+      const field = wrapper.find('small').find('a')
+      field.simulate('click')
     })
   })
 
-  describe('when action=edit', function () {
-    it('should set the text to edit', function () {
-      const wrapper = shallowRender(<LoginForm action="edit" />)
+  describe('when action=Edit', () => {
+    it('should set the text to Edit', () => {
+      const wrapper = shallowRender(<LoginForm action="Edit" />)
       const field = wrapper.find('h3')
-      expect(field.text()).to.contain('edit')
+      expect(field.text()).to.contain('Edit')
     })
 
-    it('should set the button text to edit', function () {
-      const wrapper = shallowRender(<LoginForm action="edit" />)
+    it('should set the button text to Edit', () => {
+      const wrapper = shallowRender(<LoginForm action="Edit" />)
       const field = wrapper.find('[type="submit"]')
-      expect(field.prop('label')).to.eql('edit')
+      expect(field.prop('label')).to.eql('Edit')
     })
-  });
+
+
+    it('should not show the load template link', () => {
+      const wrapper = shallowRender(<LoginForm action="Edit" />)
+      const field = wrapper.find('small')
+      expect(
+        field.find('a').exists()
+      ).to.eql(false)
+    })
+  })
+
+  describe('when Create is called, and the user clicks Create', () => {
+    let spy
+
+    it('should call the submit callback', done => {
+      const wrapper = shallowRender(<LoginForm action="Create" handleFormSubmit={done} />)
+      const form = wrapper.find('form')
+
+      form.simulate('submit')
+    })
+  })
 })
