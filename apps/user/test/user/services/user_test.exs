b808936665abd1fc4e4d570c5dbb5788d500a482
nickve28 <nickvaneyk@gmail.com>
2017-01-23 21:42:53 +0100

    User service mostly compliant with error format

--- apps/user/test/user/services/user_test.exs
+++ apps/user/test/user/services/user_test.exs
@@ -5,7 +5,7 @@ defmodule User.Services.UserTest do
   alias User.Schemas
   alias User.Repo
 
-  setup_all do
+  setup do
     Repo.delete_all(Schemas.User)
     :ok
   end
@@ -19,7 +19,7 @@ defmodule User.Services.UserTest do
   test "when the user does not exist #get should return an error" do
     %{id: id} = User.RepoHelper.create_user("Nick", "foo")
 
-    assert {:error, :enotfound} = Services.User.get(id + 1)
+    assert {:error, {:enotfound, "The user could not be found", []}} = Services.User.get(id + 1)
   end
 
   test "#create should create the new user" do
@@ -28,5 +28,28 @@ defmodule User.Services.UserTest do
     assert {:ok, %{id: _, name: "Nick", password: nil}} = Services.User.create(payload)
   end
 
-  #todo better test set and at least test auth
+  describe "#authenticate" do
+    setup do
+      user = User.RepoHelper.create_user("Nick", "foo")
+      {:ok, user: user}
+    end
+
+    test "should return unauthorized if the passwords dont match" do
+      payload = %{name: "Nick", password: "Foo"}
+      details = [
+        {:password, "no match"}
+      ]
+
+      assert {:error, {:unauthorized, "The request is not authorized", details}} === Services.User.authenticate(payload)
+    end
+
+    test "should return unauthorized if the username does not exist" do
+      payload = %{name: "nick", password: "foo"}
+      details = [
+        {:username, "not found"}
+      ]
+
+      assert {:error, {:unauthorized, "The request is not authorized", details}} === Services.User.authenticate(payload)
+    end
+  end
 end
