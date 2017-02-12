46d7fcc4b5782b663c276a158fe6a2c5c3e38ca3
nickve28 <nickvaneyk@gmail.com>
2017-02-12 11:23:23 +0100

    Added select exercise component and basic integration
    This commit adds a generic and tested select exercise component. It has been created and the progress page can return progress by id now. just shows stringified data right now

---
+++ apps/api/web/static/js/components/exercises/select.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import _ from 'lodash'
+
+import {MenuItem, SelectField} from 'material-ui';
+
+const renderItems = exercises => {
+  return _.map(exercises, exercise => {
+    const {id, name} = exercise
+    return (
+      <MenuItem
+        key={id}
+        value={id}
+        primaryText={name}
+      />
+    )
+  })
+}
+
+export default ({exercises, selectedExercise, onSelect}) => {
+
+  return (
+    <SelectField
+      floatingLabelText="Select an exercise"
+      value={selectedExercise}
+      onChange={(event, index, value) => onSelect(value)}
+    >
+      {renderItems(exercises)}
+    </SelectField>
+  )
+}
--- apps/api/web/static/js/containers/progress.js
+++ apps/api/web/static/js/containers/progress.js
@@ -1,28 +1,56 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
-import {fetchProgress} from '../actions/index'
+import {fetchProgress, fetchExercises} from '../actions/index'
+
+import _ from 'lodash'
+
+import SelectExercise from '../components/exercises/select'
 
 class Progress extends Component {
   constructor(props) {
     super(props)
+
+    this.state = {selectedExercise: null}
   }
 
   componentDidMount() {
-    this.props.fetchProgress({exerciseId: 244})
+    this.props.fetchExercises()
+  }
+
+  onSelect(selectedExercise) {
+    this.setState({selectedExercise})
+    this.props.fetchProgress({exerciseId: selectedExercise})
   }
 
   render() {
-    const {progress} = this.props
-    if (!progress) {
+    const {progress, exercises} = this.props
+    const exerciseOverview = exercises.exercises;
+
+    if (_.isEmpty(exerciseOverview)) {
       return <div>Loading...</div>
     }
-    return <div>{JSON.stringify(this.props.progress)}</div> //<ProgressOverview progress={progress} />
+    return (
+      <div>
+        <div style={{marginBottom: '10px'}} />
+
+        <div style={{marginBottom: '50px'}}>
+          <h3 style={{display: 'inline'}}>Progress</h3>
+        </div>
+        <SelectExercise exercises={exerciseOverview}
+          selectedExercise={this.state.selectedExercise}
+          onSelect={selectedExercise => this.onSelect(selectedExercise)}
+        />
+        <p>
+          {JSON.stringify(this.props.progress || {})}
+        </p>
+      </div>
+    )
   }
 }
 
-const mapStateToProps = ({progress}) => {
-  return {progress}
+const mapStateToProps = ({progress, exercises}) => {
+  return {progress, exercises}
 }
 
-export default connect(mapStateToProps, {fetchProgress})(Progress)
+export default connect(mapStateToProps, {fetchProgress, fetchExercises})(Progress)
 
