08850768b39ebbef0e00f8c4878970cb445d87f6
Nick van Eijk <nickve28@users.noreply.github.com>
2017-02-13 21:01:36 +0100

    Implemented date range filter for progress on FE (#21)

---
+++ apps/api/web/static/js/components/select_date_range.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import _ from 'lodash'
+import moment from 'moment'
+
+import {DatePicker} from 'material-ui'
+
+const formatDate = date => {
+  return moment(date).format('YYYY-MM-DD')
+}
+
+export default ({startDate, endDate, onSelect}) => {
+
+  if (!startDate || !endDate) {
+    return <div />
+  }
+
+  return (
+    <div>
+      <label>Start date</label>
+      <DatePicker
+        name="startDate"
+        formatDate={formatDate}
+        onChange={(e, val) =>  {
+          //e = empty event, 2nd arg = date
+          const date = formatDate(val)
+          onSelect({from: date})
+        }}
+        container="inline"
+        mode="landscape"
+        defaultDate={moment(startDate).toDate() || moment().toDate()}
+      />
+      <label>End date</label>
+      <DatePicker
+        name="endDate"
+        formatDate={formatDate}
+        onChange={(e, val) =>  {
+          //e = empty event, 2nd arg = date
+          const date = formatDate(val)
+          onSelect({until: date})
+        }}
+        container="inline"
+        mode="landscape"
+        defaultDate={moment(endDate).toDate() || moment().toDate()}
+      />
+    </div>
+  )
+}
