91c4330a6bf616f5460b40a696a216b12cd96d7b
Nick van Eijk <nickve28@users.noreply.github.com>
2017-03-22 22:03:06 +0100

    Improvement/mobile buttons (#39)
    * Mobile content is now 100% and change add exercise button for mobile
    
    * Implement separate save button for workout mobile

--- apps/api/web/static/js/components/workout_form.js
+++ apps/api/web/static/js/components/workout_form.js
@@ -1,10 +1,17 @@
 import React, {Component} from 'react'
 import { Field, FieldArray } from 'redux-form'
 import {Link} from 'react-router'
-import {TextField, RaisedButton} from 'material-ui'
+import {TextField, RaisedButton, FloatingActionButton} from 'material-ui'
+import ContentSave from 'material-ui/svg-icons/content/save'
 
 import ReactCSSTransitionGroup from 'react-addons-css-transition-group'
 import PerformedExerciseFields from './workouts/performed_exercise_fields'
+import NewWorkoutAction from './workouts/new_workout_action'
+
+import MediaQuery from 'react-responsive'
+
+const SMALL_DEVICE_QUERY = '(max-device-width: 1024px)'
+const LARGE_DEVICE_QUERY = '(min-device-width: 1024px)'
 
 import DatePicker from './datepicker'
 
@@ -37,6 +44,7 @@ class WorkoutForm extends Component {
 
     this.renderPerformedExercises = this.renderPerformedExercises.bind(this)
     this.onExerciseChange = this.onExerciseChange.bind(this)
+    this.renderSaveBtn = this.renderSaveBtn.bind(this)
   }
 
   onExerciseChange(exerciseId, index, fields) {
@@ -76,7 +84,7 @@ class WorkoutForm extends Component {
             />
           })}
         </ReactCSSTransitionGroup>
-        <a className="add-exercise" href="javascript:void(0);" style={{marginBottom: '5px'}} onClick={() => fields.push(EMPTY_EXERCISE)}>+ Add Exercise</a><br />
+        <NewWorkoutAction onClick={() => fields.push(EMPTY_EXERCISE)} />
       </span>
     )
   }
@@ -98,6 +106,22 @@ class WorkoutForm extends Component {
   renderError(error, key) {
     if (!_.get(error, key)) { return '' }
     return <div className="error-text">{error[key]}</div>
+  }
+
+  renderSaveBtn() {
+    return (
+      <div>
+        <MediaQuery query={SMALL_DEVICE_QUERY}>
+          <FloatingActionButton className="save-workout-btn" type="submit">
+            <ContentSave />
+          </FloatingActionButton>
+        </MediaQuery>
+
+        <MediaQuery query={LARGE_DEVICE_QUERY}>
+          <RaisedButton style={{marginTop: '10px'}} label={this.props.action} primary={true} type="submit" />
+        </MediaQuery>
+      </div>
+    )
   }
 
   render() {
@@ -131,7 +155,7 @@ class WorkoutForm extends Component {
 
           <FieldArray name="performedExercises" component={this.renderPerformedExercises} />
           {this.renderError(errors, 'uniqueExerciseError')}
-          <RaisedButton style={{marginTop: '10px'}} label={this.props.action} primary={true} type="submit" />
+          {this.renderSaveBtn()}
       </form>
       </div>
     )
