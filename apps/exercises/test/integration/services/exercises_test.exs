fa9e37adc73410867cc5051a00cf7916e6fe829b
Nick van Eijk <nickvaneijk@MacBook-Pro-van-Nick.local>
2017-05-01 20:48:48 +0200

    Split exercise insert tests

--- apps/exercises/test/integration/services/exercises_test.exs
+++ apps/exercises/test/integration/services/exercises_test.exs
@@ -66,64 +66,6 @@ defmodule ExercisesTest do
     assert {:ok, exercise} === Services.Exercise.get(id)
   end
 
-  @tag :create
-  test "#insert should fail if no name is given" do
-    payload = %{description: "foo", categories: ["bar"], type: "strength"}
-
-    assert {:error, {:invalid, "The request was deemed invalid.", [name: :required]}}=== Services.Exercise.create(payload)
-  end
-
-  @tag :create
-  test "#insert should fail if no description is given" do
-    payload = %{name: "foo", categories: ["bar"], type: "strength"}
-
-    assert {:error, {:invalid, "The request was deemed invalid.", [description: :required]}} === Services.Exercise.create(payload)
-  end
-
-  @tag :create
-  test "#insert should fail if no categories are given" do
-    payload = %{name: "foo", description: "bar", type: "strength"}
-
-    assert {:error, {:invalid, "The request was deemed invalid.", [categories: :required]}} === Services.Exercise.create(payload)
-  end
-
-  @tag :create
-  test "#insert should fail if no type is given" do
-    payload = %{name: "foo", description: "bar", categories: ["a"]}
-
-    assert {:error, {:invalid, "The request was deemed invalid.", [type: :required]}} === Services.Exercise.create(payload)
-  end
-
-  @tag :create
-  test "#insert should fail if invalid type is given" do
-    payload = %{name: "foo", description: "bar", categories: ["a"], type: "gamma"}
-
-    assert {:error, {:invalid, "The request was deemed invalid.", [type: :invalid_value]}} === Services.Exercise.create(payload)
-  end
-
-  @tag :create
-  test "#insert should save the exercise" do
-    payload = %{name: "foo", categories: ["bar"], description: "baz", type: "strength"}
-
-    assert {:ok, %{id: _}} = Services.Exercise.create(payload)
-  end
-
-  @tag :create
-  test "#insert should normalize the categories" do
-    payload = %{name: "foo", categories: ["bar"], description: "baz", type: "strength"}
-    expected = ["Bar"]
-
-    assert {:ok, %{categories: ^expected}} = Services.Exercise.create(payload)
-  end
-
-  @tag :create
-  test "#insert should normalize the name" do
-    payload = %{name: "foo", categories: ["bar"], description: "baz", type: "strength"}
-    expected = "Foo"
-
-    assert {:ok, %{name: ^expected}} = Services.Exercise.create(payload)
-  end
-
   @tag :delete
   test "#delete should return :enotfound when no exercise is found", %{exercise: %{id: id}} do
     assert {:error, {:enotfound, _, []}} = Services.Exercise.delete(%{id: id + 1})
