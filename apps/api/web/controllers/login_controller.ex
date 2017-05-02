1d86fa8def7c009fa5998af2dd061f17c3dae6b4
Nick van Eijk <nickvaneijk@MacBook-Pro-van-Nick.local>
2017-05-02 20:16:24 +0200

    Fix authentication controller endpoint
    this broke due to the new struct usage

--- apps/api/web/controllers/login_controller.ex
+++ apps/api/web/controllers/login_controller.ex
@@ -5,7 +5,9 @@ defmodule Api.LoginController do
   def authenticate(conn, %{"name" => name, "password" => password}) do
     user_with_token = User.Services.User.authenticate(%{name: name, password: password})
     case user_with_token do
-      {:ok, user} -> render conn, %{data: Map.drop(user_with_token, [:password])} #todo test controller and split properly by responsibility
+      {:ok, user} ->
+        user = user |> Map.from_struct |> Map.take([:id, :name, :token])
+        render conn, %{data: user} #todo test controller and split properly by responsibility
       {:error, {:unauthorized, message, details}} ->
         details_map = Enum.into(details, %{})
         error = %{code: 401, message: message, details: details_map}
