3e75d416c7ac1a1abf342121bd5e50f4eb54afc8
nickve28 <nickvaneyk@gmail.com>
2017-06-19 19:14:57 +0200

    add unauthorized contract to middleware of absinthe, correct format

--- apps/api/web/middleware/current_user.ex
+++ apps/api/web/middleware/current_user.ex
@@ -1,15 +1,20 @@
 defmodule Api.Web.Middleware.CurrentUser do
   @behaviour Absinthe.Middleware
 
+  @unauthorized %{
+    code: 401,
+    message: "Request not authorized",
+    details: []
+  }
+
   def call(resolution, _config) do
     context = resolution.context
-    IO.inspect(context)
     case context do
       %{user_id: id} ->
         resolution
       _ ->
         resolution
-        |> Absinthe.Resolution.put_result({:error, "unauthenticated"})
+        |> Absinthe.Resolution.put_result({:error, @unauthorized})
     end
   end
 end
