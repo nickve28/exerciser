257ff8f71933ba623504b5b19c17370c557814c9
nickve28 <nickvaneyk@gmail.com>
2017-06-24 20:59:26 +0200

    created own delete button to remove bootstrap dependency

---
+++ apps/api/web/static/js/components/button/danger.js
@@ -0,0 +1,4 @@
+import React from 'react'
+
+export default ({ children }) =>
+  <button className="button button-danger">{children}</button>
--- apps/api/web/static/js/sections/exercises/components/exercise_view_item.js
+++ apps/api/web/static/js/sections/exercises/components/exercise_view_item.js
@@ -2,9 +2,11 @@ import React from 'react'
 
 import { Link } from 'react-router'
 
+import DangerButton from 'components/button/danger'
+
 export default ({ exercise }) => {
   return  <li className="list-view-group-item" key={exercise.id}>
     <Link to={`/exercises/${exercise.id}`}>{exercise.name}</Link>
-    <button className="btn btn-warning btn-danger btn-xs">x</button>
+    <DangerButton>x</DangerButton>
   </li>
 }
