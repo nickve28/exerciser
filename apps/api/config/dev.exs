3908ee12a4adabcea1fbec0b86356fe60182898a
nickve28 <nickvaneyk@gmail.com>
2017-07-27 20:27:56 +0200

    Remove remaining JS traces such as babel files

--- apps/api/config/dev.exs
+++ apps/api/config/dev.exs
@@ -13,8 +13,7 @@ config :api, Api.Endpoint,
     patterns: [
       ~r{priv/static/.*(js|css|png|jpeg|jpg|gif|svg)$},
       ~r{priv/gettext/.*(po)$},
-      ~r{web/views/.*(ex)$},
-      ~r{web/templates/.*(eex)$}
+      ~r{web/views/.*(ex)$}
     ]
   ]
 
