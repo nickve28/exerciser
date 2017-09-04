a227cbc445d2944375aa41cbb114d7489cf0d941
nickve28 <nickvaneyk@gmail.com>
2017-09-04 20:42:27 +0200

    Bump timex 2 minor versions more

--- apps/workout/mix.exs
+++ apps/workout/mix.exs
@@ -40,7 +40,7 @@ defmodule Workout.Mixfile do
       {:postgrex, ">= 0.13.0"},
       {:ecto, "~> 2.1.0"},
       {:poolboy, "~> 1.5"},
-      {:timex, "3.1.21"},
+      {:timex, "3.1.23"},
       {:timex_ecto, "3.1.1"},
       {:vex, "~> 0.6.0"}
     ]
